93bbe13443d26e32b96cec9a7582c080d25d7e7d
Billy <billyjrsantos@outloook.com>
2026-06-04 12:34:59 -0300

    refactor: Otimizando memória

--- saxpy/saxpy.hpp
+++ saxpy/saxpy.hpp
@@ -5,19 +5,13 @@
 #include <stdexcept>
 
 namespace algebra {
-
-/**
- * Realiza a operação Y = a * X + Y.
- * Segue o princípio KISS: direto ao ponto, sem classes desnecessárias.
- */
     inline void apply_saxpy(float multiplier, const std::vector<float>& input_x, std::vector<float>& result_y) {
         if (input_x.size() != result_y.size()) {
             throw std::invalid_argument("Vector sizes must be identical for SAXPY operation.");
         }
 
         for (std::size_t i = 0; i < input_x.size(); ++i) {
-            float multiplied_x = multiplier * input_x[i];
-            result_y[i] = multiplied_x + result_y[i];
+            result_y[i] = multiplier * input_x[i] + result_y[i];
         }
     }
 
